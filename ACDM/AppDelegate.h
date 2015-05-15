bbcfcae72cc895b617cba6b70033557ce7d84059
LiJunliang <Lee@LiJunliangdeMacBook-Pro.local>
2015-05-15 10:27:01 +0800

    Initial Commit

---
+++ ACDM/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ACDM
+//
+//  Created by LiJunliang on 15/5/15.
+//  Copyright (c) 2015年 wondersgroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ACDM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ACDM
+//
+//  Created by LiJunliang on 15/5/15.
+//  Copyright (c) 2015年 wondersgroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
